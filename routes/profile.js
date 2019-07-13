6b0a9b2a1d29448036e0595df66141857b6cb786
Amine TELLI <atelli@e1r9p19.42.fr>
2019-07-13 19:21:25 +0200

    Frontend profile bug resolu

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const session = require('express-session');
 const crypto = require('crypto-js');
+const formidable = require('formidable');
 const fs = require('fs');
 const mod = require('./mod');
 let	  today = new Date();
