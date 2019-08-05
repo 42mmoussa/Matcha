7be37314f22704e15b9219304e6a03914a2d77c4
Mohamad <mohamadmoussa88@yahoo.fr>
2019-08-05 16:10:57 +0200

    end search, need location and popularity to finish it

---
+++ bower_components/js/multirange.js
@@ -0,0 +1,116 @@
+(function() {
+    "use strict";
+    
+    var supportsMultiple = self.HTMLInputElement && "valueLow" in HTMLInputElement.prototype;
+    
+    var descriptor = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, "value");
+    
+    var multirange = function(input) {
+        if (supportsMultiple || input.classList.contains("multirange")) {
+            return;
+        }
+    
+        var value = input.getAttribute("value");
+        var values = value === null ? [] : value.split(",");
+        var min = +(input.min || 0);
+        var max = +(input.max || 100);
+        var ghost = input.cloneNode();
+    
+        input.classList.add("multirange", "original");
+        ghost.classList.add("multirange", "ghost");
+    
+        input.value = values[0] || min + (max - min) / 2;
+        ghost.value = values[1] || min + (max - min) / 2;
+    
+        input.parentNode.insertBefore(ghost, input.nextSibling);
+    
+        Object.defineProperty(input, "originalValue", descriptor.get ? descriptor : {
+            // Fuck you Safari >:(
+            get: function() { return this.value; },
+            set: function(v) { this.value = v; }
+        });
+    
+        Object.defineProperties(input, {
+            valueLow: {
+                get: function() { return Math.min(this.originalValue, ghost.value); },
+                set: function(v) { this.originalValue = v; },
+                enumerable: true
+            },
+            valueHigh: {
+                get: function() { return Math.max(this.originalValue, ghost.value); },
+                set: function(v) { ghost.value = v; },
+                enumerable: true
+            }
+        });
+    
+        if (descriptor.get) {
+            // Again, fuck you Safari
+            Object.defineProperty(input, "value", {
+                get: function() { return this.valueLow + "," + this.valueHigh; },
+                set: function(v) {
+                    var values = v.split(",");
+                    this.valueLow = values[0];
+                    this.valueHigh = values[1];
+                    update();
+                },
+                enumerable: true
+            });
+        }
+    
+        if (typeof input.oninput === "function") {
+            ghost.oninput = input.oninput.bind(input);
+        }
+    
+        function update() {
+            ghost.style.setProperty("--low", 100 * ((input.valueLow - min) / (max - min)) + 1 + "%");
+            ghost.style.setProperty("--high", 100 * ((input.valueHigh - min) / (max - min)) - 1 + "%");
+        }
+    
+        ghost.addEventListener("mousedown", function passClick(evt) {
+            // Are the ghost and input elements inverted? (ghost is lower range)
+            var isInverted = input.valueLow == ghost.value;
+            // Find the horizontal position that was clicked (as a percentage of the element's width)
+            var clickPoint = evt.offsetX / this.offsetWidth;
+            // Map the percentage to a value in the range (note, assumes a min value of 0)
+            var clickValue = max * clickPoint;
+    
+            // Get the distance to both high and low values in the range
+            var highDiff = input.valueHigh - clickValue;
+            var lowDiff = Math.abs(input.valueLow - clickValue);
+    
+            if (isInverted? highDiff < lowDiff : lowDiff < highDiff) {
+                // The low value is closer to the click point than the high value
+                // We should update the low value input
+                var passEvent = new MouseEvent("mousedown", evt);
+                // Pass a new event to the low "input" element (which is obscured by the
+                // higher "ghost" element, and doesn't get mouse events outside the drag handle
+                input.dispatchEvent(passEvent);
+                // The higher "ghost" element should not respond to this event
+                evt.preventDefault();
+                return false;
+            }
+        });
+        input.addEventListener("input", update);
+        ghost.addEventListener("input", update);
+    
+        update();
+    }
+    
+    multirange.init = function() {
+        [].slice.call(document.querySelectorAll("input[type=range][multiple]:not(.multirange)")).forEach(multirange);
+    }
+    
+    if (typeof module === "undefined") {
+        self.multirange = multirange;
+        if (document.readyState == "loading") {
+            document.addEventListener("DOMContentLoaded", multirange.init);
+        }
+        else {
+            multirange.init();
+        }
+    } else {
+        module.exports = multirange;
+    }
+    
+    })();
+    
--- routes/search.js
+++ routes/search.js
@@ -15,6 +15,7 @@ router.get('/:page', function (req, res) {
 	let results = {};
 	
 	if (req.session.connect) {
+		
 		if (!isNaN(req.params.page) && req.params.page > 0) {
 
 			let nbElementOnPage = 20;
@@ -55,10 +56,14 @@ router.get('/:page', function (req, res) {
 
 			let strAge = req.query.age;
 
-			if (typeof strAge === 'string') {
-				let tags = strAge.split(' ');
-				let from = mod.ageToDate(tags[1]);
-				let to = mod.ageToDate(tags[0]);
+			if (strAge !== undefined && (strAge[0] != 0 || strAge[1] != 100)) {
+				let from = mod.ageToDate(parseInt(strAge[0]));
+				let to = mod.ageToDate(parseInt(strAge[1]) + 1);
+				if (from > to) {
+					let c = from;
+					from = to;
+					to = c;
+				}
 				if (i === 0) {
 					search += " WHERE (birthday BETWEEN ? AND ?)";
 					i = 1;
@@ -71,30 +76,42 @@ router.get('/:page', function (req, res) {
 			}
 
 			let first = 0;
-			if (searchData.length > 0) {
+
+			let order = req.query.order;
+			const lstOrder = [
+				"age",
+				"popularity"
+			]
+			console.log(typeof order);
+			if (lstOrder.includes(order)) {
+				if (order === "age") {
+					search += " ORDER BY birthday DESC";
+				}
+			}
+			else if (searchData.length > 0) {
 				searchCol.forEach(function (element) {
 					if (first === 0) {
-            if (element === 'birthday') {
-              search += " ORDER BY ( IF ((birthday BETWEEN ? AND ?) , 1, 0)";
-              searchData.push(searchData[first]);
-              first++;
-              searchData.push(searchData[first]);
-            } else {
-              search += " ORDER BY ( IF (tags LIKE ? , 1, 0)";
-              searchData.push(searchData[first]);
-            }
+						if (element === 'birthday') {
+							search += " ORDER BY ( IF ((birthday BETWEEN ? AND ?) , 1, 0)";
+							searchData.push(searchData[first]);
+							first++;
+							searchData.push(searchData[first]);
+						} else {
+							search += " ORDER BY ( IF (tags LIKE ? , 1, 0)";
+							searchData.push(searchData[first]);
+						}
 					} else {
-            if (element === 'birthday') {
-              search += " + IF ((birthday BETWEEN ? AND ?) , 1, 0)";
-              searchData.push(searchData[first]);
-              first++;
-              searchData.push(searchData[first]);
-            } else {
-              search += " + IF (tags LIKE ? , 1, 0)";
-              searchData.push(searchData[first]);
-            }
+						if (element === 'birthday') {
+							search += " + IF ((birthday BETWEEN ? AND ?) , 1, 0)";
+							searchData.push(searchData[first]);
+							first++;
+							searchData.push(searchData[first]);
+						} else {
+							search += " + IF (tags LIKE ? , 1, 0)";
+							searchData.push(searchData[first]);
+						}
 					}
-          first++;
+					first++;
 				});
 				search += ") DESC";
 			}
@@ -109,31 +126,44 @@ router.get('/:page', function (req, res) {
 					console.log(search + " | " + searchData);
 					return conn.query(search, searchData);
 				})
-				.then(row => {
-					if (row.length == 0) {
-						return res.redirect("/search/");
-					}
-					conn.end();
-					let i = -1;
-					let today = new Date();
-					while (++i < row.length) {
-						let bday = new Date(row[i].birthday);
-						row[i].age = mod.dateDiff(bday, today);
-						if (row[i].tags != null) {
-							row[i].tags = row[i].tags.replace(/,/g, ' ');
+				.then(row => {					
+					if (row.length != 0) {
+						let i = -1;
+						let today = new Date();
+						while (++i < row.length) {
+							let bday = new Date(row[i].birthday);
+							row[i].age = mod.dateDiff(bday, today);
+							if (row[i].tags != null) {
+								row[i].tags = row[i].tags.replace(/,/g, ' ');
+							}
 						}
+						results = row;
 					}
-					results = row;
 					return conn.query("SELECT * FROM tags ORDER BY nb_tag ASC");
 				})
-				.then(tags => {					
+				.then(tags => {
+					conn.end();
+					if (typeof results.length === "undefined") {
+						return res.render('search', {
+							tags: tags,
+							nbTags: tags.length,
+							users: results,
+							nbUsers: 0,
+							page: 1,
+							count: 0,
+							popup: true,
+							popupMsg: "no user found",
+							popupTitle: "Search"
+						});
+					}
 					return res.render('search', {
 						tags: tags,
 						nbTags: tags.length,
 						users: results,
 						nbUsers: results.length,
 						page: page + 1,
-						count: Math.ceil(results[0].count / nbElementOnPage)
+						count: Math.ceil(results[0].count / nbElementOnPage),
+						filters: req.query
 					});
 				})
 				.catch(err => {
