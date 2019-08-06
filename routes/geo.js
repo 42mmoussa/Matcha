f13763fc783958b3a22f1663a26b20282902b335
Mohamad <mohamadmoussa88@yahoo.fr>
2019-08-06 14:20:23 +0200

    geolocalisation

--- routes/geo.js
+++ routes/geo.js
@@ -1,11 +1,13 @@
 const express           = require('express');
 var router              = express.Router();
-var ip                  = require("ip").address();
 const iplocation		= require("iplocation").default;
 const keys		        = require("./keys");
 
-
 router.get('/', function (req, res) {
+    var ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
+            req.connection.remoteAddress || 
+            req.socket.remoteAddress || 
+            req.connection.socket.remoteAddress;
     iplocation(ip, [], (error, res1) => {
         let location = res1.city;
         return res.render('geo', {
