75139508f2bdfc310fe72e3d0522e33cc59a4dd8
Mohamad <mohamadmoussa88@yahoo.fr>
2019-08-06 12:12:06 +0200

    ip location test on server none localhost

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const crypto = require('crypto-js');
-const mod = require('./mod');
-var today = new Date();
-const passport = require('passport');
-const passportSetup = require('./oauth');
+const express			= require('express');
+const router			= express.Router();
+const crypto			= require('crypto-js');
+const mod				= require('./mod');
+var today				= new Date();
+const passport			= require('passport');
+const passportSetup		= require('./oauth');
+const iplocation		= require("iplocation").default;
 
 router.get('/', function (req, res) {
 	if (req.session.connect) {
@@ -83,6 +84,15 @@ router.post('/login_validation', function(req, res) {
 			return conn.query("SELECT * FROM users WHERE (username = ? OR email=?) AND pwd = ?", [login, login, pwd]);
 		})
 		.then((result) => {
+			var ip = req.headers['x-forwarded-for'] || 
+						req.connection.remoteAddress || 
+						req.socket.remoteAddress ||
+						(req.connection.socket ? req.connection.socket.remoteAddress : null);
+			console.log(ip);
+			iplocation(ip, [], (error, res1) => {
+				console.log(res1);
+				
+			})
 			if (result[0] === undefined)
 				throw "bad password or username";
 			if (result[0].confirm === 1) {
