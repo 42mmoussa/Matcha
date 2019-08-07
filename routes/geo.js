6f93daeb0e28ab2bed8e83f3cb294a1bf2956fad
Mohamad <mohamadmoussa88@yahoo.fr>
2019-08-07 12:24:21 +0200

    /map

--- routes/geo.js
+++ routes/geo.js
@@ -2,20 +2,43 @@ const express           = require('express');
 var router              = express.Router();
 const iplocation		= require("iplocation").default;
 const keys		        = require("./keys");
+const mod				= require('./mod');
 
 router.get('/', function (req, res) {
-    var ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
-            req.connection.remoteAddress || 
-            req.socket.remoteAddress || 
-            req.connection.socket.remoteAddress;
-    iplocation(ip, [], (error, res1) => {
-        let location = res1.city;
-        return res.render('geo', {
-            ip: ip,
-            location: location,
-            key: keys.google.key
+    if (req.session.connect) {
+        var ip = (req.headers['x-forwarded-for'] || '').split(',').pop() || 
+                req.connection.remoteAddress || 
+                req.socket.remoteAddress || 
+                req.connection.socket.remoteAddress;
+    
+        
+        mod.pool.getConnection()
+        .then(conn => {
+            conn.query("USE matcha")
+            .then(() => {
+                return conn.query("SELECT * FROM profiles;")
+            })
+            .then((row) => {
+                iplocation(ip, [], (error, res1) => {
+                    let location = res1;                   
+                    conn.end();
+                    return res.render('geo', {
+                        users: row,
+                        ip: ip,
+                        location: location,
+                        key: keys.google.key
+                    });
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                conn.end();
+                return res.render("/maps");
+            });
         });
-    });
+    } else {
+        return res.redirect("/login")
+    }
 });
 
 module.exports = router;
