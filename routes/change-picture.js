774e620463395ffef12d8cc8f1c8e6c8845213ab
Amine <aminetelli@gmail.com>
2019-08-10 17:10:22 +0200

    upload pictures done

--- routes/change-picture.js
+++ routes/change-picture.js
@@ -60,6 +60,19 @@ router.get('/', function(req, res) {
 
 router.post('/upload', function (req, res) {
 	if (req.session.connect) {
+		var deleteFolderRecursive = function(path) {
+			if( fs.existsSync(path) ) {
+			  fs.readdirSync(path).forEach(function(file,index){
+				var curPath = path + "/" + file;
+				if(fs.lstatSync(curPath).isDirectory()) { // recurse
+				  deleteFolderRecursive(curPath);
+				} else { // delete file
+				  fs.unlinkSync(curPath);
+				}
+			  });
+			  fs.rmdirSync(path);
+			}
+		};
 		function checkFileType(file, cb){
 			// Allowed ext
 			const filetypes = /jpeg|jpg|png|gif/;
@@ -71,7 +84,18 @@ router.post('/upload', function (req, res) {
 			if(mimetype && extname){
 			return cb(null,true);
 			} else {
-			cb('Error: Images Only!');
+				if (!fs.existsSync("img/" + req.session.user.id)) {
+					deleteFolderRecursive("img/" + req.session.user.id);
+					mod.pool.getConnection()
+					.then((conn) => {
+						conn.query("USE matcha;")
+						.then(() => {
+							conn.query("UPDATE profiles SET pictures = 0 WHERE id_usr = ?;", [req.session.user.id]);
+						});
+						conn.end();
+					});
+				}
+				cb('Error: Images Only!');
 			}
 		}
 		// Set The Storage Engine
@@ -81,10 +105,43 @@ router.post('/upload', function (req, res) {
 		if (!fs.existsSync("img/" + req.session.user.id)) {
 			fs.mkdirSync("img/" + req.session.user.id);
 		}
+		var i = 0;
 		const storage = multer.diskStorage({
 			destination: "img/" + req.session.user.id,
 			filename: function(req, file, callback){
-				callback(null, "profile.jpg");
+				if (file) {
+					if (i == 0) {
+						mod.pool.getConnection()
+						.then((conn) => {
+							conn.query("USE matcha;")
+							.then(() => {
+								conn.query("SELECT * FROM profiles WHERE id_usr = ?", [req.session.user.id])
+								.then(rows => {
+									if (rows[0].pictures == 0)
+										conn.query("UPDATE profiles SET pictures = 10 WHERE id_usr = ?;", [req.session.user.id]);
+								});
+							});
+							conn.end();
+						});
+						callback(null, "profile.jpg");
+					}
+					else {
+						mod.pool.getConnection()
+						.then((conn) => {
+							conn.query("USE matcha;")
+							.then(() => {
+								conn.query("SELECT * FROM profiles WHERE id_usr = ?", [req.session.user.id])
+								.then(rows => {
+									if (rows[0].pictures >= 10 && rows[0].pictures < 14)
+										conn.query("UPDATE profiles SET pictures = pictures + 1 WHERE id_usr = ?;", [req.session.user.id]);
+								});
+							});
+							conn.end();
+						});
+						callback(null, "image" + i + ".jpg");
+					}
+					i++;
+				}
 			}
 			});
 			
@@ -95,7 +152,13 @@ router.post('/upload', function (req, res) {
 			fileFilter: function(req, file, cb){
 				checkFileType(file, cb);
 			}
-			}).single('myImage');
+			}).fields([
+				{name: "image0", maxCount: 1},
+				{name: "image1", maxCount: 1},
+				{name: "image2", maxCount: 1},
+				{name: "image3", maxCount: 1},
+				{name: "image4", maxCount: 1},
+			]);
 			
 			// Check File Type
 		upload(req, res, (err) => {
@@ -105,21 +168,7 @@ router.post('/upload', function (req, res) {
 				});
 			}
 			else {
-				if(req.file == undefined){
-				res.render('change-picture', {
-					error: 'Error: No File Selected!'
-				});
-				} else {
-					mod.pool.getConnection()
-					.then((conn) => {
-						conn.query("USE matcha;")
-						.then(() => {
-							conn.query("UPDATE profiles SET pictures = pictures + 10 WHERE id_usr = ?;", [req.session.user.id]);
-						});
-						conn.end();
-						res.redirect('/profile');
-					});
-				}
+				res.redirect('/profile');
 			}
 		});
 	}
